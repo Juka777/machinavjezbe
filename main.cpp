b4384234c85fc16b608ffd072090ef93b6156928
Juka777 <git@github.com:Juka777/machinavjezbe.git>
2019-05-01 18:54:15 +0200

    Pointers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<string>
 
 int main()
 {
+	int number = 5;
+	int& tracker = number;
+	int* ptr = &number;
+	std::string* stringptr;
+	std::string someWord = "Nesto";
+	stringptr = &someWord;
+	int** doubleptr = &ptr;
+
+
+	number = 9;
 	std::cout << "Hello world!" << std::endl;
+	std::cout << "number : " << number<< std::endl;
+	std::cout << "tracker : " << tracker<< std::endl;
+	std::cout << "ptr : " << ptr << std::endl;
+	std::cout << "stringptr : " << stringptr << std::endl;
+
+	std::cout << "derefference stringptr:" << *stringptr << std::endl;
+	*stringptr = "nesto drugo";
+	std::cout << "derefference stringptr take 2 :" << *stringptr << std::endl;
+	std::cout << "doubleptr  :" << *doubleptr << std::endl;
 	std::cin.get();
 }
